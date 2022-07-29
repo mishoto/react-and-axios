0af6f730baae460be747d6c09f4d808d899317b0
mishoto <mihail.dev.io@gmail.com>
2022-07-29 07:28:06 +0300

    update/AJS-1.0.0-add simple get request

--- AxiosWithJS/src/examples/FirstRequest.jsx
+++ AxiosWithJS/src/examples/FirstRequest.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const url = 'http://course-api.com/react-store-products';
 
 const FirstRequest = () => {
-  return (
-    <div>FirstRequest</div>
-  )
-}
+  const fetchData = async () => {
+    try{
+      const response = await axios(url);
 
-export default FirstRequest
+      console.log(response);
+    } catch (error){
+      console.log(error.response);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <h2 className='text-center'>First Request</h2>;
+};
+
+export default FirstRequest;
