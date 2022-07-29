7f8c2d2271c356c53a303fe60dcfc9213f51d20f
mishoto <mihail.dev.io@gmail.com>
2022-07-29 08:50:23 +0300

    update/AJM-1.0.0- add interceptors logic

--- AxiosWithJS/src/examples/Interceptors.jsx
+++ AxiosWithJS/src/examples/Interceptors.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import authFetch from '../axios/interceptors';
 
 const Interceptors = () => {
-  return (
-    <div>Interceptors</div>
-  )
-}
+  const fetchData = async () => {
+    try {
+      const resp = await authFetch('/react-store-productss');
+      console.log(resp);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
-export default Interceptors
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <h2 className='text-center'>interceptors</h2>;
+};
+export default Interceptors;
